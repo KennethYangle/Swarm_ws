e0dca9a7a1cd6cb6b02c0d71c4c57b6c45cf80fc
yangkun <1269769296@qq.com>
2020-12-05 15:28:13 +0800

    update

--- decision/scripts/test.py
+++ decision/scripts/test.py
@@ -4,12 +4,7 @@
 
 import math
 import numpy as np
+from swarm_msgs.msg import Pipeline
 
-
-search_pos = np.array([[110, 3, 3],
-                       [150, 4.5, 3],
-                       [244, 1.2, 3],
-                       [244, 20, 3],
-                       [160, 18, 3],
-                       [170, 11, 3]])
-print(search_pos[1])
+a = Pipeline()
+print(a)
