7e498bb1c5fb3368bff1f716f742a04bf414f997
yangkun <1269769296@qq.com>
2020-12-07 14:47:24 +0800

    test Pipeline

--- decision/scripts/test.py
+++ decision/scripts/test.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-
-import math
+import rospy
 import numpy as np
-from swarm_msgs.msg import Pipeline
+from swarm_msgs.msg import Pipeline, Pipeunit
+from geometry_msgs.msg import Point32
 
-a = Pipeline()
-print(a)
+class Decision:
+    def __init__(self):
+        rospy.init_node('decision_node', anonymous=True)
+        self.pipe_pub = rospy.Publisher('decision_info', Pipeline , queue_size=10)
+        self.rate = rospy.Rate(1)
+
+    def worker(self):
+        a = Pipeline()
+        a.pipetype.data = 10
+        u = Pipeunit()
+        u.middle = Point32(10,20,30)
+        u.left = Point32(0,20,30)
+        u.right = Point32(20,20,30)
+        while not rospy.is_shutdown():
+            a.units.append(u)
+            print(a)
+            self.pipe_pub.publish(a)
+            self.rate.sleep()
+
+
+if __name__ == '__main__':
+    dd = Decision()
+    dd.worker()
