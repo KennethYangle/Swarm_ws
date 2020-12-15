ae9c643f3651124dcb99810a7da52ea2fbc5356c
yangkun <1269769296@qq.com>
2020-12-15 20:51:52 +0800

    calculate corrected local position

---
+++ decision/scripts/origin_bias.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import rospy
+import os
+import argparse
+import numpy as np
+from geometry_msgs.msg import PoseStamped, TwistStamped, Point32
+param_id = 0
+param_num = 0
+local_pos_bia = Point32()
+
+def local_pos_cb(msg):
+    global local_pos_pub, param_num, param_id,local_pos_bia
+
+    cloum = (param_num+1)//2
+    if param_id > cloum:
+        bias_ue4_x = 2
+    else:
+        bias_ue4_x = 0
+    bias_ue4_y = 2 * ((param_id -1) % cloum)
+
+    local_pos_bia.x = msg.pose.position.x + bias_ue4_y
+    local_pos_bia.y = msg.pose.position.y + bias_ue4_x
+    local_pos_bia.z = msg.pose.position.z
+    local_pos_pub.publish(local_pos_bia)
+
+
+if __name__ == '__main__':
+    # global param_num, param_id
+    rospy.init_node('pose_cor', anonymous=True)
+    param_id = rospy.get_param("~drone_id")
+    param_num = rospy.get_param("~drone_num")
+    # bias = Origin_bias(param_id,param_num)
+    local_pos_pub = rospy.Publisher('/drone_%s/mavros/local_position/pose_cor'%(param_id), Point32, queue_size=10)
+    local_pos_sub = rospy.Subscriber('/drone_%s/mavros/local_position/pose'%(param_id), PoseStamped, local_pos_cb)
+    rospy.spin()
+    
+    # while True:
+    #     bias.start()
+    # bias.start()
+    print("Finish")
