7484c74e192cac59c7b4a11d1f5224aaf51ce46a
yangkun <1269769296@qq.com>
2020-12-15 23:04:45 +0800

    readme is refresh

--- decision/scripts/origin_bias.py
+++ decision/scripts/origin_bias.py
@@ -12,14 +12,16 @@ local_pos_bia = Point32()
 
 def local_pos_cb(msg):
     global local_pos_pub, param_num, param_id,local_pos_bia
-
+    
+    #以及UE4生成的飞机法则进行编号，如果ue4生成方式变化，这里也需要编号
     cloum = (param_num+1)//2
     if param_id > cloum:
         bias_ue4_x = 2
     else:
         bias_ue4_x = 0
     bias_ue4_y = 2 * ((param_id -1) % cloum)
-
+    
+    #坐标转换，ue4_y对应mavros_x，ue4_x对应mavros_y
     local_pos_bia.x = msg.pose.position.x + bias_ue4_y
     local_pos_bia.y = msg.pose.position.y + bias_ue4_x
     local_pos_bia.z = msg.pose.position.z
